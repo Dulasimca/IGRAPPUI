0d7282b8b2a62c9de5c431c1d1cd0f91ae53abfe
Adithya_10010 <Adithya_10010@TANDVBL0037>
2022-08-25 17:11:19 +0530

    firm reg and soc reg changes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CcRequestListComponent } from './E-Services/certified-copy/cc-request-list/cc-request-list.component';
 import { EcSearchAndApplyCCComponent } from './Forms-Module/E-Services/Encumbrance-Certificate/ec-search-and-apply-cc/ec-search-and-apply-cc.component';
 import { EcViewECComponent } from './Forms-Module/E-Services/Encumbrance-Certificate/ec-view-ec/ec-view-ec.component';
+import { FirmRegistartionComponent } from './Forms-Module/Registration/firm-registartion/firm-registartion.component';
+import { SocietyRegistrationComponent } from './Registration/society-registration/society-registration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'encumbrance-search', component: EcSearchAndApplyCCComponent },
   { path: 'encumbrance-viewEC', component: EcViewECComponent },
+  { path: 'firm-registration', component: FirmRegistartionComponent},
+  { path: 'societ-registration', component: SocietyRegistrationComponent},
+  { path: 'request-list', component: CcRequestListComponent}
 
 ];
 
